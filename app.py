d469308d19a108238196745288f6870b0ba7674f
Anand Talware <anandtalware27@gmail.com>
2024-12-03 23:26:36 +0530

    updated flask code

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def index():
 if __name__ == '__main__':
     if not os.path.exists('static/uploads'):
         os.makedirs('static/uploads')
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
